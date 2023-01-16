3be2fc7059ea81b053c7bb4edbb4a6db1e8f878d
Thomas8483 <thomas.jaxel@gmail.com>
2023-01-16 13:39:42 +0100

    Création de SelectorActivity.kt

--- app/src/main/java/fr/isen/jaxel/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/jaxel/androiderestaurant/HomeActivity.kt
@@ -1,11 +1,27 @@
 package fr.isen.jaxel.androiderestaurant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    fun EntreesBtnOnClick(view: View) {
+        Toast.makeText(this, "Entrées cliqué", Toast.LENGTH_SHORT).show();
+        val gameActivityIntent = Intent(this@HomeActivity, SelectorActivity::class.java);
+        startActivity(gameActivityIntent);
+    }
+
+    fun PlatsBtnOnClick(view: View) {
+        Toast.makeText(this, "Plats cliqué", Toast.LENGTH_SHORT).show();
+    }
+    fun DessertsBtnOnClick(view: View) {
+        Toast.makeText(this, "Desserts cliqué", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/fr/isen/jaxel/androiderestaurant/SelectorActivity.kt
@@ -0,0 +1,11 @@
+package fr.isen.jaxel.androiderestaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selector)
+    }
+}
