7eac59992710f4a486937ddb84cb15718f682518
Thomas8483 <thomas.jaxel@gmail.com>
2023-01-26 14:01:31 +0100

    Affichage des images associées aux plats

--- app/src/main/java/fr/isen/jaxel/androiderestaurant/DishAdapter.kt
+++ app/src/main/java/fr/isen/jaxel/androiderestaurant/DishAdapter.kt
@@ -3,14 +3,17 @@ package fr.isen.jaxel.androiderestaurant
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import fr.isen.jaxel.androiderestaurant.model.Items
+import com.squareup.picasso.Picasso
 
 internal class DishAdapter(private var myArrayList: ArrayList<Items>, val onItemClickListener: () -> Unit) : RecyclerView.Adapter<DishAdapter.MyViewHolder>() {
 
     internal inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val dishTitle: TextView = view.findViewById(R.id.dishTitle)
+        val contentImage: ImageView = view.findViewById(R.id.image_meal)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -22,6 +25,11 @@ internal class DishAdapter(private var myArrayList: ArrayList<Items>, val onItem
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val item = myArrayList[position]
         holder.dishTitle.text = item.nameFr
+        if (item.images[0].isNotEmpty()) {
+            Picasso.get().load(myArrayList[position].images[0])
+                .placeholder(R.drawable.android_logo)
+                .into(holder.contentImage)
+        }
         holder.itemView.setOnClickListener {
             onItemClickListener()
         }
